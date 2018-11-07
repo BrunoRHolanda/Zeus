c31d641007e1ea14f455247d21a0650478866215
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2018-11-07 11:33:00 -0300

    criando projeto

---
+++ wink.com/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wink.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
